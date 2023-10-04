437c3c332c7578c34ab04acbf7c7fcdf64bca7fa
EricBernardes <ericbernardes70@gmail.com>
2023-10-04 03:16:47 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define tam 6
+
+// void ordenaInsDireta(int *v) {
+//   int aux;
+//   int j;
+//   for (int i = 1; 1 < (tam - 1); i++) {
+//     aux = v[i];
+//     j = --i;
+//     while (j >= 0 && aux < v[i]) {
+//       v[j + 1] = v[j];
+//       j--;
+//     }
+//     if (j != (i - 1)) {
+//       v[j + 1] = aux;
+//     }
+//   }
+// }
+
+void ordenaInsDireta(int *v) {
+  int aux;
+  int j;
+  for (int i = 1; i < tam; i++) {
+    aux = v[i];
+    j = i - 1;
+    while (j >= 0 && aux < v[j]) {
+      v[j + 1] = v[j];
+      j--;
+    }
+    if (j != (i - 1)) {
+      v[j + 1] = aux;
+    }
+  }
+}
+
+void imprimeVetor(int *vetor) {
+  for (int i = 0; i < tam; i++)
+    printf("%d  ", vetor[i]);
+  printf("\n");
+}
+
+void main() {
+  int v[tam] = {5, 2, 4, 6, 1, 3};
+  ordenaInsDireta(v);
+  imprimeVetor(v);
+}
